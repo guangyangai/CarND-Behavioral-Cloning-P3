616109029edcfce6c2161d39eac757df76ad90c3
Guang Yang <guang@celect.com>
2018-02-11 16:08:05 -0500

    add in BGR to RGB conversion

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import os
 import csv
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Activation, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.callbacks import ModelCheckpoint
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,9 +26,13 @@ def generator(samples, batch_size=32):
 				center_image_name = 'data/IMG/'+batch_sample[0].split('/')[-1]
 				left_image_name = 'data/IMG/'+batch_sample[1].split('/')[-1]
 				right_image_name = 'data/IMG/'+batch_sample[2].split('/')[-1]
-				center_image = cv2.imread(center_image_name)
-				left_image = cv2.imread(left_image_name)
-				right_image = cv2.imread(right_image_name)
+				center_image_original = cv2.imread(center_image_name)
+				left_image_original = cv2.imread(left_image_name)
+				right_image_original = cv2.imread(right_image_name)
+				#convert from BGR to RGB
+				center_image = cv2.cvtColor(center_image_original,cv2.COLOR_BGR2RGB)
+				left_image = cv2.cvtColor(left_image_original,cv2.COLOR_BGR2RGB)
+				right_image = cv2.cvtColor(right_image_original,cv2.COLOR_BGR2RGB)
 				center_angle = float(batch_sample[3])
 				left_angle = center_angle + CORRECTION
 				right_angle = center_angle - CORRECTION
@@ -60,31 +65,33 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 #build model
 model = Sequential()
-#normalizing
-model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 #cropping
 model.add(Cropping2D(cropping=((70,25), (0,0))))
+#normalizing
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 #network
 model.add(Convolution2D(6,5,5, activation = 'relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(16,5,5, activation = 'relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
 model.add(Dense(1))
-#add dropout?
-# model.add(Dropout())
+
 
 model.compile(loss='mse', optimizer='adam')
 
-#save history
+
+#use callback to save history
+filepath = 'weights.{epoch:02d}-{val_loss:.2f}.hdf5'
+model_checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+
 history_object = model.fit_generator(train_generator, samples_per_epoch =
 	len(train_samples), validation_data = 
 	validation_generator,
 	nb_val_samples = len(validation_samples), 
-	nb_epoch=5, verbose=1)
+	nb_epoch=10, verbose=1, callbacks=[model_checkpoint])
 
-#save the model
 model.save('model.h5')
-
